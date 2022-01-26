acd8c211c1d339368d38f198c3da6b58cf05bf37
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2022-01-27 00:15:04 +0100

    delete bootsrap and add styling to navbar

--- website/static/assets/gulpfile.js
+++ website/static/assets/gulpfile.js
@@ -1,38 +1,23 @@
-var browserSync = require('browser-sync').create();
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('node-sass'));
-var sourcemaps = require('gulp-sourcemaps');
-
-
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
 const paths = {
-    styles: {
-        src: './scss/**/*.scss',
-        dest: './css'
-  }
-};
+    src: './scss/**/*.scss',
+    dest: './css'
+}
 
-// Compile SCSS
-gulp.task('scss', function() {
-    return gulp.src([paths.styles.src])
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(browserSync.stream());
-});
+function style() {
+    return gulp.src(paths.src)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.dest))
+        .pipe(browserSync.stream())
+}
 
-// Minify CSS
-// gulp.task('minify:css', function() {
-//     return gulp.src([
-//             paths.src.css + '/soft-design-system.css'
-//         ])
-//         .pipe(cleanCss())
-//         .pipe(rename(function(path) {
-//             // Updates the object in-place
-//             path.extname = ".min.css";
-//         }))
-//         .pipe(gulp.dest(paths.src.css))
-// });
+function watch() {
+    gulp.watch('./scss/**/*.scss', style);
+}
 
-// Default Task: Compile SCSS and minify the result
-gulp.task('default', gulp.series('scss'));
+
+exports.style = style;
+exports.watch = watch;
+gulp.task('default', watch)
